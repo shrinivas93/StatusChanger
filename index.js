0dd2db1d381c17e922b2a1f52ca82cac9d6f6e3a
madhurabhad <47741559+madhurabhad@users.noreply.github.com>
2023-06-09 13:10:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send(Object.keys(map).map((key) => ({ key, value: map[key], status: getStatus(value) })));
+  res.send(Object.keys(map).map((key) => ({ key, value: map[key], status: getStatus(map[key]) })));
 });
 
 app.post("/", (req, res) => {
